01f9d24829cee9bfeb296c19396e4fa2c711122c
yuanhuiwu <yuanhuiwu@gmail.com>
2011-11-02 00:56:04 +0000

    i18n

--- db/mysql5-3.0.x update to 3.0.2.sql
+++ db/mysql5-3.0.x update to 3.0.2.sql
@@ -1,27 +1,10 @@
-drop table if exists lang;
-
-drop table if exists lang_lbl;
-
 /*==============================================================*/
-/* Table: lang                                                  */
+/* Table: i18n                                                  */
 /*==============================================================*/
-create table lang
+create table i18n
 (
    lang                 varchar(20) not null,
-   des                  varchar(128) not null,
-   primary key (lang)
-);
-
-/*==============================================================*/
-/* Table: lang_lbl                                              */
-/*==============================================================*/
-create table lang_lbl
-(
-   lang                 varchar(20) not null,
-   id                  varchar(255) not null,
+   id                   varchar(255) not null,
    lbl                  varchar(255) not null,
    primary key (lang, id)
 );
-
-alter table lang_lbl add constraint FK_Reference_12 foreign key (lang)
-      references lang (lang) on delete restrict on update restrict;
